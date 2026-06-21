7de4a1ff95a0fbd2c2c89fa61f3d298d0cc4cadc
Adam Walls <avvall@google.com>
2026-06-21 03:48:57 +0000

    Remove extraneous files

--- cl_03_foundational_tests.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-git restore --source=add-thread-based-memory-allocation-hooks \
-  cobalt/memory/cobalt_memory_attribution_manager_unittest.cc
--- cl_09_browsertests.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-git restore --source=add-thread-based-memory-allocation-hooks \
-  cobalt/browser/memory_attribution_browsertest.cc \
-  cobalt/testing/browser_tests/BUILD.gn
